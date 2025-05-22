bed9bb07683386434c56856b951ae29e71e0fc5a
Beyda Nur Pınarbaşı <beydanur.pinarbasi@gmail.com>
2025-05-23 00:30:26 +0300

    vercel wpa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Menu, X } from "lucide-react"; // ikonlar için lucide-react kullanıyoruz
 
 export default function Navbar() {
